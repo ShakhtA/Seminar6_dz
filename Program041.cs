1572a35faed4ebe051b2c9a8ece17b7f0780c505
Andrey <shakht_a@mail.ru>
2023-03-14 21:49:07 +0300

    домашнее задание семинар 6

---
+++ Program041.cs
@@ -0,0 +1,75 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223 -> 3
+
+//--------------------------БЛОК ОПИСАНИЙ-------------------------------------
+int GetSizeFromUser(string num_message, string error_message)
+{
+    int num;
+    while (true)
+    {
+     try
+        {
+            Console.Write(num_message);
+            num = int.Parse(Console.ReadLine() ?? "");
+            if (num > 0)
+                return num;
+            Console.WriteLine("Ошибка ввода!");
+        }
+    catch
+        {
+            Console.WriteLine(error_message);
+        }
+    }
+}
+
+int[] GetArray(int size, string error_message)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = GetNumberFromUser("Ведите число: ", "Ошибка ввода!");
+    }
+    return arr;
+}
+
+int GetNumberFromUser(string num_message, string error_message)
+{
+    int num;
+    while (true)
+    {
+     try
+        {
+            Console.Write(num_message);
+            num = int.Parse(Console.ReadLine() ?? "");
+            return num;
+        }
+        catch
+        {
+            Console.WriteLine(error_message);
+        }
+    }   
+}
+
+int CountPositiveNumber(int[] arr)
+{
+    int count = 0;
+    foreach (int el in arr)
+        count += el > 0 ? 1:0;
+    return count;
+}
+
+void PrintResult(int[] arr, int count)
+{
+    Console.WriteLine($"{string.Join(", ", arr)} -> {count}");
+    Console.WriteLine();
+}
+//-----------------------------------------------------------------------------
+Console.Clear();
+
+// Функция запрашивает у пользователя количество (М) чисел
+int M = GetSizeFromUser("Введите положительное число: ", "Ошибка ввода!"); 
+
+int[] array = GetArray(M, "Ошибка ввода!"); // Функция запрашивает у пользователя М чисел и заносит их в массив
+int count = CountPositiveNumber(array);     // Функция возвращает кол-во чисел, больших, чем 0
+PrintResult(array, count);                  // Вывод результата на экран
---
+++ Program043.cs
@@ -0,0 +1,49 @@
+﻿/* Напишите программу, которая найдёт точку пересечения двух прямых,
+   заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+   значения b1, k1, b2 и k2 задаются пользователем.
+   b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+//------------------БЛОК ОПИСАНИЙ------------------------------
+double GetNumberFromUser(string num_message, string error_message)
+{
+    double num;
+    while (true)
+    {
+        Console.Write(num_message);
+        if (double.TryParse(Console.ReadLine(), out num))
+            return num;
+        Console.WriteLine(error_message);
+    }   
+}
+
+double[] GetCoord(double k1, double b1, double k2, double b2)
+{
+    double[] arr = new double[2];
+    arr[0] = (b2 - b1) / (k1 - k2);
+    arr[1] = k1 * arr[0] + b1;
+    double y = k2 * arr[0] + b2;
+    return arr;
+}
+
+void PrintResult(double k1, double b1, double k2, double b2, double[] arr)
+{
+    Console.Write($"k1 = {k1}, b1 = {b1}, k2 = {k2}, b2 = {b2} -> ");
+    Console.Write($"({arr[0]:f1}; {arr[1]:f1})");
+    Console.WriteLine();
+}
+
+//---------------------------------------------------------------
+
+Console.Clear();
+
+// Запрос данных у пользователя
+double k1 = GetNumberFromUser("Введите коэффициент k1 - ", "Ошибка ввода!");    
+double b1 = GetNumberFromUser("Введите свободный член b1 - ", "Ошибка ввода!");
+double k2 = GetNumberFromUser("Введите коэффициент k2 - ", "Ошибка ввода!");
+double b2 = GetNumberFromUser("Введите свободный член b2 - ", "Ошибка ввода!");
+
+double[] coord = GetCoord(k1, b1, k2, b2);
+PrintResult(k1, b1, k2, b2, coord);
+Console.WriteLine();
+
